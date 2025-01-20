03313a01c5dd49869efef8511d6f24b73c2f7b53
Dan Gilleland <dgilleland@nait.ca>
2025-01-20 13:35:57 -0700

    node repl explorations

---
+++ src/005/Jan20.js
@@ -0,0 +1,39 @@
+const pi = 3.14159
+let firstNumber = 5
+let secondNumber
+secondNumber = 8
+firstNumber + secondNumber
+firstNumber - secondNumber
+firstNumber * secondNumber
+firstNumber / secondNumber
+firstNumber % secondNumber
+secondNumber % firstNumber
+0.1 + 0.1 + 0.1
+typeof firstNumber
+firstNumber = 'five'
+typeof firstNumber
+firstNumber = {} // an empty object
+typeof firstNumber
+firstNumber = true
+typeof firstNumber
+firstNuber = []
+typeof firstNumber
+firstNumber = []
+typeof firstNumber
+// let's just reset our variable to a string valueOf
+firstNumber = '5'
+firstNumber + secondNumber
+firstNumber = parseInt(firstNumber)
+firstNumber + secondNumber
+let temp
+temp = parseFloat('5.95')
+temp
+parseInt('5.95')
+pi
+pi.toString()
+pi.toFixed(4)
+pi.toFixed(2)
+firstNumber.toFixed(2)
+Number(firstNumber)
+Number('4.34')
+new Number('2.23')
