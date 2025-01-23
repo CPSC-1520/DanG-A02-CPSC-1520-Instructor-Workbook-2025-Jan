2a730bb8ee9f78583a610de769ce39c1d42f904b
Dan Gilleland <dgilleland@nait.ca>
2025-01-23 10:53:20 -0700

    Some notes and practice with event listeners and handlers

--- src/008/soft-intro/js/main.js
+++ src/008/soft-intro/js/main.js
@@ -1,18 +1,29 @@
 console.log('JavaScript file loaded!');
 
+// This is an example of a function that will be my Event Handler.
+// It contains the code that I want to run when a certain event happens.
 const handleClick = function(event) {
+    event.preventDefault(); // don't do your default behaviour
     // TODO: Use for exploration purposes
-    const target = event.target;
+    const target = event.target;    // DOM element
     document.getElementById('feedback').innerText = `Clicked from ${target.tagName}`;
 }
 
 let heading = document.querySelector('h1');
+// I "register" my Event Handler with my DOM object by adding an
+// "event listener" which "listens" for a specific event
 heading.addEventListener('click', handleClick);
+//                       \_____/  <--- The mouse-click event
 
 // TODO: Add an event listener for the button with id "register".
 //       Listen for the 'dblclick' event.
 //       Prompt the user for their name, then for their email,
-//       then output the information to the #feedback element.
+//       then output the information to the #register element.
+document.getElementById('register') // many ways to get this DOM element
+        .addEventListener('dblclick', function(ev) {
+            // this is my anonymous function to handle the dblclick event
+            console.log('This was double-clicked');
+        });
 
 // TODO: Add another event listener for the heading, this time for
 //       the 'dblclick' event.
@@ -20,3 +31,6 @@ heading.addEventListener('click', handleClick);
 //       to the following element's property:
 //          document.querySelector('html').dataset.theme
 //       Try it out, then inspect the elements in the Dev Tools.
+heading.addEventListener('dblclick', function(evt) {
+    document.querySelector('html').dataset.theme = 'light';
+});
