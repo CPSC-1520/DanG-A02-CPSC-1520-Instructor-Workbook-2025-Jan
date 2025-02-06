566bb3b9ad32fd685a5f40ba98ce7f93525e97b4
Dan Gilleland <dgilleland@nait.ca>
2025-02-06 11:10:15 -0700

    Validating form input

--- src/012/course-marks/js/main.js
+++ src/012/course-marks/js/main.js
@@ -9,9 +9,9 @@ const outputLine = function (htmlMarkup, replace) {
   const out = document.getElementById("output");
   htmlMarkup += "<br />";
   if (replace) {
-    out.innerHTML = htmlMarkup;
+    out.innerHTML = htmlMarkup; // replace
   } else {
-    out.innerHTML += htmlMarkup;
+    out.innerHTML += htmlMarkup; // append
   }
 };
 
@@ -22,8 +22,81 @@ const outputLine = function (htmlMarkup, replace) {
 const addEvalItem = function (evt) {
   evt.preventDefault();
   // TODO: Write your exploratory code here
-  outputLine("Edit this form's event handler to compare the values");
-};
+  outputLine("Processing eval item...", true);
+
+  // get ahold of the input elements
+  // console.log(evt.target.elements);
+  let elements = evt.target.elements;
+  let inputName = elements.evalName;
+  let inputWeight = elements.weight;
+
+  let isValid = true; // Optimistic on data entry
+  let canCalculate = false; // Pessimistic
+  let message; // to build my string apart from the call to outputLine()
+
+  if(!isEmpty(inputName) && !isEmpty(inputWeight)) {
+    // All good, display the message
+    message = `The ${inputName.value} item is worth ${inputWeight.value} %.`;
+    outputLine(message);
+  } else {
+    isValid = false; // Missing critical data
+    if(isEmpty(inputName)) {
+      message = '<span class="error">You must enter a name for the evaluation item.</span>';
+      outputLine(message);
+    }
+    if(isEmpty(inputWeight)) {
+      message = '<span class="error">A weight is required for the evaluation item.</span>';
+      outputLine(message);
+    }
+  }
+
+  // TODO: Get earned/total points
+  let inputTotal = elements.totalPoints;
+  let inputEarned = elements.earnedPoints;
+
+  if(!isEmpty(inputTotal)) {
+    if(!isEmpty(inputEarned)) {
+      if(inputEarned.value > inputTotal.value) {
+        isValid = false;
+        message = "<span class='error'>You cannot earn more than the total points.</span>";
+        outputLine(message);
+      } else {
+        // Can only calculate if I have values and the earned <= total
+        canCalculate = true;
+      }
+    }
+  } else {
+    if(!isEmpty(inputEarned)) {
+      // Problem
+      isValid = false;
+      message = "<span class='error'>You've entered earned points without a total available points.</span>";
+      outputLine(message);
+    } 
+  }
+
+  // Finish my processing
+  if(isValid) {
+    outputLine('Saving evaluation item.');
+    // Clear the inputs
+    inputName.value = '';
+    inputWeight.value = '';
+
+    inputName.focus(); // Make control ready for keyboard input
+  }
+
+}; // end of addEvalItem() event handler
+
+/* Utility Functions */
+/**
+ * isEmpty() determines if the input's value (once trimmed) is an empty string.
+ * @param {HTMLInputElement} inputElement An input element to be validated
+ * @returns {boolean} - True if the trimmed value is empty, false otherwise
+ */
+const isEmpty = function(inputElement) {
+  return (inputElement.value.trim() === '');
+}
+
+
 
 /**
  * Reports information about the last state of the form before the reset was applied to the input controls.
