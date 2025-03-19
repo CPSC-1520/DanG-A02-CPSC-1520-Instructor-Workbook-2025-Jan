56e933d08ddd03a604d04a232604ae323a03da87
Dan Gilleland <dgilleland@nait.ca>
2025-03-19 11:02:46 -0600

    Update starter kit

--- sk/020/await-fetch/js/main.js
+++ sk/020/await-fetch/js/main.js
@@ -1,15 +1,16 @@
-const apiHost = 'https://deckofcardsapi.com/api/deck/';
-const shuffle = `${apiHost}/shuffle/?deck_count=1`;
-const getDrawUrl = (deckId, count) => `${apiHost}${deckId}/draw/?count=${count}`;
+const apiHost = 'https://deckofcardsapi.com/api/deck';
+const getDeckUrl = (deckId = 'new') => `${apiHost}/${deckId}/shuffle/?deck_count=1`;
+const getDrawUrl = (deckId, count) => `${apiHost}/${deckId}/draw/?count=${count}`;
 
 // This function illustrates calling the fetch API using async/await
+// Internally, this function is still a blocking function because it's using await.
 const callApi = async function(url) {
     let response = await fetch(url);
     let data = await response.json();
     return data;
 }
 
-// TODO: Follow these steps to populate the element with `id="hand"`
+// TODO: Follow these steps to use the deckofcardsapi using the callApi.
 
 // 1) Get the deck ID from a new shuffled deck (and show in element with id="deck-identifier")
 
