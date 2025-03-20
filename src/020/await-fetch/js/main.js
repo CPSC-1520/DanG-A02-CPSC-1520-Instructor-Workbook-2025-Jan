9e240d189d15ba7fb82a28b39476fa5ec38908c1
Dan Gilleland <dgilleland@nait.ca>
2025-03-20 10:44:58 -0600

    Demo of drawing cards

--- src/020/await-fetch/js/main.js
+++ src/020/await-fetch/js/main.js
@@ -13,6 +13,32 @@ const callApi = async function(url) {
 // TODO: Follow these steps to use the deckofcardsapi using the callApi.
 
 // 1) Get the deck ID from a new shuffled deck (and show in element with id="deck-identifier")
+let deck = await callApi(getDeckUrl());
+// console.log(deck);
+let deckId = deck.deck_id;
+document.querySelector('h1').innerHTML += ` <input id="deck-id" value="${deckId}" />`;
+
+document.getElementById('deck-id')
+        .addEventListener('onchange', (evt) => {
+            deckId = evt.target.value;
+        });
+
 
 // 2) Draw a single card (and append to the HTML of the element with `id="hand"`)
 //    e.g.: let html = `<img src="${card.image}" alt="${card.value} of ${card.suit}" />`;
+const addCard = function(card) {
+    let container = document.getElementById('hand');
+    let html = `<img src="${card.image}" alt="${card.value} of ${card.suit}" />`;
+    container.innerHTML += html;
+}
+
+// let result = await callApi(getDrawUrl(deckId, 2));
+// console.log(result);
+// result.cards.forEach(addCard);
+
+// 3) Hook up the button to draw cards
+document.getElementById('draw-card')
+        .addEventListener('click', async () => {
+            let result = await callApi(getDrawUrl(deckId, 1));
+            result.cards.forEach(addCard);
+        });
