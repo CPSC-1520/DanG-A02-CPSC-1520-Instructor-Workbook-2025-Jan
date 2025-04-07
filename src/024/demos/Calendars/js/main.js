bd3a2517504a006a099af3db83b0f13f48111785
Dan Gilleland <dgilleland@nait.ca>
2025-04-07 13:43:20 -0600

    Calendar has correct dates

--- src/024/demos/Calendars/js/main.js
+++ src/024/demos/Calendars/js/main.js
@@ -7,10 +7,28 @@ function buildDaySlot(date) {
     return div;
 }
 
-let calendarContainer = document.getElementById('calendar');
+// Add an event listener for the <input type="month" /> control
+document.getElementById('selected-month')
+    .addEventListener('change', buildCalendar);
 
-let today = new Date();
-for(let count = 0; count < 35; count++) {
-    today.setDate(count);
-    calendarContainer.appendChild(buildDaySlot(today));    
+
+function buildCalendar(evt) {
+    let calendarContainer = document.getElementById('calendar');
+    let selection = evt.target.value;  // '2025-04' === April, 2025
+    
+    let [year, month] = selection.split('-'); // string[]
+    // Creating a date object with year/month only defaults the date to
+    // the first date of the month
+    let today = new Date(year, month - 1);  // This could be any day of the week
+    // Adjust for when we want our calendar to start
+    today.setDate(today.getDate() - (today.getDay() + 1));
+    //            \____ 1 ______/   \___ offset _______/
+    //                                   to "before" the week begins
+
+    // Reset the calendar
+    calendarContainer.innerHTML = '';
+    for (let count = 0; count < 35; count++) {
+        today.setDate(today.getDate() + 1); // Add one day
+        calendarContainer.appendChild(buildDaySlot(today));
+    }
 }
